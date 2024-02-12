5357758ed3c4eb92edbb7eede1617fe23a074e08
amritanshu-acog <156794365+amritanshu-acog@users.noreply.github.com>
2024-02-12 11:21:16 +0530

    Create myModule.js

---
+++ myModule.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function squarert(a) {
+    return Math.sqrt(a);
+}
+
+module.exports = { add, subtract, multiply, divide, squarert };
